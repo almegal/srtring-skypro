8227f9f4740b1f1185674113bf6309cfc3274ef6
alekseipeikhel <alekseipeikhel@gmail.com>
2023-11-22 23:41:14 +0300

    string homework

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        ///task 1
+        String firstName = "Aleksei";
+        String middleName = "Ilyich";
+        String lastName = "Peikhel";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("Ф. И. О. сотрудника — %s", fullName);
 
+        //task 2
+        String fullNameUppesCase = fullName.toUpperCase();
+        System.out.printf("\nФ. И. О. сотрудника для заполнения отчета — %s", fullNameUppesCase);
+
+        //task 3
+        fullName = "Иванов Семён Семёнович";
+        String formatFullName = fullName.replaceAll("ё", "е");
+        System.out.printf("\nДанные Ф. И. О. сотрудника — %s", formatFullName);
     }
 }
